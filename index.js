fbcb5d2b3620dcf4e8b3821414fff92fe228ab85
Harsh <darshnar34@gmail.com>
2024-10-11 16:59:53 +0530

    updated index.js

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.get('/', (req, res) => {
     res.send('hi')
 })
 
-app.post('/api/pr', (req, res) => {
-    console.log(req.body)
+app.get('/api/pr', (req, res) => {
+    console.log(req.query.event);
+    console.log(req.query.pr_number);
+    console.log(req.query.action);
+
+    res.send("done");
 })
 
 app.listen(port, () => {
