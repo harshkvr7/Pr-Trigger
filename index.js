21cef0fefd8e5e1144585fc39f4ec757055dfd88
Harsh <darshnar34@gmail.com>
2024-10-12 14:32:27 +0530

    updated index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/api/pr', async (req, res) => {
     try {
         const response = await axios.get(filesUrl, {
             headers: {
-                'Authorization': `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+                'Authorization': `Bearer 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`,
                 'Accept': 'application/vnd.github.v3+json'
             }
         });
